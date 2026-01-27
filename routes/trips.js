135c93659332159c242c82e30d87d79afe0457e6
Thibaut <thibaut.visensang@orange.fr>
2026-01-27 19:50:41 +0100

    lastVersion

--- routes/trips.js
+++ routes/trips.js
@@ -1,52 +1,30 @@
-require('../models/connexion');
-const express =require("express");
+require("../models/connexion");
+const express = require("express");
 const router = express.Router();
-const Trip = require('../models/trips');
+const Trip = require("../models/trips");
 
-
-router.post('/trips', (req, res) => {
- const NewTrip = new Trip({ 
-departure: req.body.departure, 
-arrival: req.body.arrival, 
-date: req.body.date,
-price: req.body.price, });
- NewTrip.save().then(() =>{
-  Trip.find().then(data => {
-  res.json({ result: true, allTrips: data });
-  })
+router.post("/trips", (req, res) => {
+  const NewTrip = new Trip({
+    departure: req.body.departure,
+    arrival: req.body.arrival,
+    date: req.body.date,
+    price: req.body.price,
+  });
+  NewTrip.save().then(() => {
+    Trip.find().then((data) => {
+      res.json({ result: true, allTrips: data });
+    });
   });
 });
 
-
-router.get('/trips', (req, res) => {
-  Trip.find().then(data => {
-  res.json({ allTrips: data });
-});
-});
-
-
-router.delete("/:id", (req, res) => {
-  Message.findByIdAndDelete(req.params.id).then(() => {
-    res.json({ result: true });
+router.get("/trips", (req, res) => {
+  Trip.find({
+    departure: req.body.departure,
+    arrival: req.body.arrival,
+    date: req.body.date,
+  }).then((data) => {
+    res.json({ allTrips: data });
   });
 });
-
-
-// on va y arriver le Thib
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
